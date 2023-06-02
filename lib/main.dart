aca8ff967549da9ab3f4fe648d22cf836cffe797
wpinrui <77185900+wpinrui@users.noreply.github.com>
2023-06-02 18:38:55 +0800

    Complete app

--- lib/main.dart
+++ lib/main.dart
@@ -11,29 +11,84 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.yellow[800],
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width: 100,
-                color: Colors.red,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const CircleAvatar(
+                radius: 80.0,
+                backgroundImage: AssetImage('images/profile.jpg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
-                  )
-                ],
+              const Text(
+                'Kang Seulgi 강슬기',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                    fontFamily: 'Pacifico'),
               ),
-              Container(
-                width: 100,
-                color: Colors.blue,
+              const Text(
+                'RED VELVET LEAD DANCER',
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SizedBox(
+                  width: 200,
+                  height: 20,
+                  child:
+                      Divider(color: Colors.yellowAccent[100], thickness: 1.5),
+                ),
+              ),
+              Card(
+                margin:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+                color: Colors.white,
+                child: ListTile(
+                  minLeadingWidth: 10,
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.yellow.shade900,
+                  ),
+                  title: Text(
+                    '81234567',
+                    style: TextStyle(
+                      color: Colors.yellow.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: const EdgeInsets.symmetric(vertical: 0, horizontal: 25),
+                color: Colors.white,
+                child: ListTile(
+                  minLeadingWidth: 10,
+                  leading: Icon(
+                    Icons.alternate_email,
+                    color: Colors.yellow.shade900,
+                  ),
+                  title: Text(
+                    'kang-seulgi@redvelvet.com',
+                    style: TextStyle(
+                      color: Colors.yellow.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
